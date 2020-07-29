aee1247e06260732a34167fd46389199bf348460
Jason Belgard <belgard.jason@gmail.com>
2020-07-29 00:23:32 -0500

    13, 14, 15

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,9 +10,20 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # YOUR CODE HERE
 
+with open("src/foo.txt") as file:
+  print(file.read())
+  file.close()
+
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make 
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+
+with open("bar.txt", "w") as file:
+  file.writelines(["Hey there\n", "My name is Jason\n", "What is your name?\n"])
+
+with open('bar.txt') as file:
+  print(file.read())
+  file.close()
